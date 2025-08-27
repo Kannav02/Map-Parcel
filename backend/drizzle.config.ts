d8d831c10fcd296e63397b0b663441ebebd0d2d9
Kannav02 <kannavsethi02@gmail.com>
2025-08-26 23:19:32 -0400

    chore(backend): config file fixed

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -3,12 +3,14 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+console.log('DATABASE_URL exists:', !!process.env.DATABASE_URL)
+
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
-    ssl:'require' 
+    url: process.env.DATABASE_URL+ '?sslmode=require',
   },
+
 })
